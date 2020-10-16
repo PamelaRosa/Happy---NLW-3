d677a93d6d1d7073b74a7558606b36528e0dd43e
PamelaRosa <pa.rosa4@gmail.com>
2020-10-16 14:43:54 -0300

    Listando os orfanatos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,7 +5,10 @@ const routes = Router();
 
 //MVC -> Model/Views/Controllers
 
+//Métodos comuns: index, show, create, update, delete
+
 //Criar orfanatos
+routes.get('/orphanages', OrphanagesController.index);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
