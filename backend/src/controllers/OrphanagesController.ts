a741017e1b289136247274ee1984c68ee238ae07
PamelaRosa <pa.rosa4@gmail.com>
2020-10-16 23:28:27 -0300

    Validação de dados e tratamento de erro de validação

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 import Orphanage from '../models/Orphanage';
 
@@ -47,8 +48,7 @@ export default {
             return { path: image.filename } //path é a unica informação que necessita ser preenchida, o resto é preenchido automaticamente
         })
 
-        //Somente cria o orfanato, não salva no banco
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -57,7 +57,30 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        };
+
+        //Validações dos campos feitas com o Yup
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
         });
+
+        await schema.validate(data, {
+            abortEarly: false, // Para retornar TODOS os campos que estam com erros
+        });
+
+        //Somente cria o orfanato, não salva no banco
+        const orphanage = orphanagesRepository.create(data);
 
         //Para salvar o orfanato criado no banco de dados
         await orphanagesRepository.save(orphanage);
